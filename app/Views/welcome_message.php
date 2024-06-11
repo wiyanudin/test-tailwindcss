7fcd2717a9a8dcfcf53044fa8ff43cc1481fdfd0
wiyanudin <wiyanudinw@gmail.com>
2024-06-11 15:48:00 +0700

    first commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?= base_url('assets/css/output.css'); ?>">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1 class="text-3xl font-bold underline text-red-400 bg-green-400 text-center">
+    Hello world!
+  </h1>
+</body>
+
+</html>
